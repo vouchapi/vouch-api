ee23fcdc7e0b8fd14085161cf78a781bb3af8fd6
Siyam Hosan <siyam.hosan@yahoo.com>
2023-10-24 11:04:23 +0600

    temporary things

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,17 +1,16 @@
 import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config';
 import { EventEmitterModule } from '@nestjs/event-emitter';
+import { AppController } from './api/app/app.controller';
+import { Version1Controller } from './api/v1/version1.controller';
+import { LicenseService } from './cache/license.cache';
+import { ProfileService } from './cache/profile.cache';
+import { VouchService } from './cache/vouch.cache';
 import { DbConfig } from './config';
 import { DrizzleModule } from './drizzle/drizzle.module';
 import { EventsModule } from './events/events.module';
 import { ClientAuthMiddleware } from './middleware/client.auth';
 import { AppAuthMiddleware } from './middleware/app.auth';
-import { TempService } from './temp.service';
-import { LicenseService } from './cache/license.cache';
-// import { AppController } from './api/app/app.controller';
-// import { Version1Controller } from './api/v1/version1.controller';
-// import { ProfileService } from './cache/profile.cache';
-// import { VouchService } from './cache/vouch.cache';
 
 @Module({
   imports: [
@@ -23,9 +22,8 @@ import { LicenseService } from './cache/license.cache';
     }),
     EventsModule
   ],
-  providers: [TempService, LicenseService]
-  // controllers: [AppController, Version1Controller],
-  // providers: [ProfileService, VouchService, LicenseService]
+  controllers: [AppController, Version1Controller],
+  providers: [ProfileService, VouchService, LicenseService]
 })
 export class AppModule implements NestModule {
   configure(consumer: MiddlewareConsumer) {
